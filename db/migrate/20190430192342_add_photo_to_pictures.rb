173db436b069062b730d6cdc346e85e5d85e021b
Andrew Post <andykpost@gmail.com>
2019-04-30 20:30:30 +0000

    Getting error, db migration

--- db/migrate/20190430192342_add_photo_to_pictures.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToPictures < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pictures, :photo, :string
-  end
-end
--- db/migrate/20190430194401_add_picture_to_photos.rb
+++ db/migrate/20190430194401_add_picture_to_photos.rb
@@ -1,5 +1,13 @@
-class AddPictureToPhotos < ActiveRecord::Migration[5.2]
+class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
-    add_column :photos, :picture, :string
+    create_table :captions do |t|
+      t.text :caption
+      t.integer :user_id
+      t.integer :place_id
+      t.timestamps
+    end
+
+    add_index :captions, [:user_id, :place_id]
+    add_index :captions, :place_id
   end
 end
