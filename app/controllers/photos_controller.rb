cf826a33b3c6afc998598697c05b173dc77d5cf8
Andrew Post <andykpost@gmail.com>
2019-05-09 20:41:05 +0000

    Created mailer

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,7 +12,7 @@ class PhotosController < ApplicationController
 private
 
   def photo_params
-    params.require(:picture).permit(:caption)
+    params.require(:photo).permit(:caption, :picture)
   end
 
 end
